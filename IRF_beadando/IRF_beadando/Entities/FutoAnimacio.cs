d8a143510731419e0939194477d2107b931629d4
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-11-24 14:39:22 +0100

    8. - FutoAnimacio osztály létrehozása commit

---
+++ IRF_beadando/IRF_beadando/Entities/FutoAnimacio.cs
@@ -0,0 +1,17 @@
+﻿using IRF_beadando.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadando
+{
+	public class FutoAnimacio
+	{
+		public Futo CreateNew()
+		{
+			return new Futo();
+		}
+	}
+}
--- IRF_beadando/IRF_beadando/Form2.cs
+++ IRF_beadando/IRF_beadando/Form2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_beadando.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,10 +15,19 @@ namespace IRF_beadando
 	{
 		Database1Entities4 context = new Database1Entities4();
 		Felhasznalo felhasznalo;
+
+		private List<Futo> futos = new List<Futo>();
+
+		private FutoAnimacio _animacio;
+		public FutoAnimacio Animacio
+		{
+			get { return _animacio; }
+			set { _animacio = value; }
+		}
 		public Form2(Felhasznalo bejelentkezettFelhasznalo)
 		{
 			InitializeComponent();
-
+			Animacio = new FutoAnimacio();
 			this.felhasznalo = bejelentkezettFelhasznalo;
 		}
 	}
