41985e7690a03100ac09e8a0161782ddbf6fc9ba
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-11-24 14:20:08 +0100

    7. - Futo osztály létrehozása commit

---
+++ IRF_beadando/IRF_beadando/Entities/Futo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_beadando.Entities
+{
+	public class Futo: Label 
+	{
+		public Futo()
+		{
+			AutoSize = false;
+			Width = 50;
+			Height = Width;
+			Paint += Futo_Paint;
+		}
+
+		private void Futo_Paint(object sender, PaintEventArgs e)
+		{
+			DrawImage(e.Graphics);
+		}
+
+		protected void DrawImage(Graphics g)
+		{
+			Image imageFile = Image.FromFile("Images/runner.png");
+			g.DrawImage(imageFile, new Rectangle(0,0,Width,Height));
+		}
+
+		public void MoveFuto()
+		{
+			Left += 1;
+		}
+	}
+}
--- IRF_beadando/IRF_beadando/Form2.Designer.cs
+++ IRF_beadando/IRF_beadando/Form2.Designer.cs
@@ -29,10 +29,18 @@ namespace IRF_beadando
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.SuspendLayout();
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Tan;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "Form2";
 			this.Text = "Form2";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
