7fac6573ec98b3c127b034caa0e4c29539a0f9f1
Anna Kovács <anna.kovacs6@stud.uni-corvinus.hu>
2020-11-25 12:51:49 +0100

    15. - excel header, adatok betöltése tömbbe, excelhez getcell elkészítése commit

--- IRF_beadando/IRF_beadando/Form2.cs
+++ IRF_beadando/IRF_beadando/Form2.cs
@@ -24,7 +24,18 @@ namespace IRF_beadando
 		List<Nyar_koszonto_futas> Nyar_Koszonto_Futas;
 		List<Mikulas_futas> Mikulas_Futas;
 
+		Excel.Application xlApp;
+		Excel.Workbook xlWB;
+		Excel.Worksheet xlSheet;
+
 		private List<Futo> _futos = new List<Futo>();
+
+		string[] headers = new string[]{
+				"Esemény",
+				"Táv",
+				"Helyezés",
+				"Futóazonosító",
+				"Idő" };
 
 		private FutoAnimacio _animacio;
 		public FutoAnimacio Animacio
@@ -135,6 +146,79 @@ namespace IRF_beadando
 		private void btnMentes_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		public void CreateExcel()
+		{
+			try
+			{
+				xlApp = new Excel.Application();
+
+				xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+				xlSheet = xlWB.ActiveSheet;
+
+				//CreateTable();
+
+				xlApp.Visible = true;
+				xlApp.UserControl = true;
+
+			}
+			catch (Exception ex)
+			{
+				string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+				MessageBox.Show(errMsg, "Error");
+
+				xlWB.Close(false, Type.Missing, Type.Missing);
+				xlApp.Quit();
+				xlWB = null;
+				xlApp = null;
+			}
+		}
+
+		public void CreateTable()
+		{
+
+			for (int i = 1; i < headers.Length; i++)
+			{
+				xlSheet.Cells[1, i] = headers[i-1];
+			}
+
+			object[,] values = new object[Budapest_10Kms.Count, headers.Length];
+
+			int counter = 0;
+			foreach (Budapest_10km bp in Budapest_10Kms)
+			{
+				values[counter, 0] = (from x in context.Esemeny
+									  where x.ESEMENY_ID == bp.ESEMENY_FK
+									  select x.NEV).FirstOrDefault();
+				values[counter, 1] = bp.TAV;
+				values[counter, 2] = bp.HELYEZES;
+				values[counter, 3] = (from x in context.Felhasznalo
+									  where x.FELH_NEV == bp.FELH_NEV_FK
+									  select x.FUTO_AZONOSITO).FirstOrDefault();
+				values[counter, 4] = bp.IDO;
+				counter++;
+			}
+		}
+
+		private string GetCell (int x, int y)
+		{
+			string ExcelCoordinate = "";
+			int dividend = y;
+			int modulo;
+
+			while (dividend>0)
+			{
+				modulo = (dividend - 1) % 26;
+				ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+				dividend = (int)((dividend - modulo) / 26);
+
+			}
+
+			ExcelCoordinate += x.ToString();
+
+			return ExcelCoordinate;
 		}
 	}
 }
